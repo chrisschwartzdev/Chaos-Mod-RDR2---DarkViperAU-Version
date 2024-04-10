29c2b92e257def625fffb70f9c068bdd2e3ae623
Chris <cschwartz110@gmail.com>
2024-04-09 19:22:54 -0500

    Include optional 'hideBlip' parameter in peds.h

--- src/Effects/peds.h
+++ src/Effects/peds.h
@@ -876,6 +876,6 @@ void RemoveAllPedWeapons(Ped ped);
 
 void MarkPedAsCompanion(Ped ped);
 
-void MarkPedAsEnemy(Ped ped);
+void MarkPedAsEnemy(Ped ped, bool hideBlip = false);
 
 void FixEntityInCutscene(Entity entity);
